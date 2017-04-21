33225cde89a655824941a02ee744a8b4e0771fe2
nathanpartridge11 <nathanpartridge11@gmail.com>
2017-04-21 09:15:26 -0600

    Open the camera

--- app/src/main/java/com/teamenfuego/barcodeshopper/barcodeshopper/MainActivity.java
+++ app/src/main/java/com/teamenfuego/barcodeshopper/barcodeshopper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.teamenfuego.barcodeshopper.barcodeshopper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                openCamera();
+
             }
         });
 
@@ -97,5 +99,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void openCamera()
+    {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
