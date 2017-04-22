afb4b3b8f94fbae1c99f84b9fb5e67ed75f560c1
Yrish <Yrish3@gmail.com>
2017-04-21 20:16:44 -0600

    implements barcode lookup

--- app/src/main/java/com/teamenfuego/barcodeshopper/barcodeshopper/Item.java
+++ app/src/main/java/com/teamenfuego/barcodeshopper/barcodeshopper/Item.java
@@ -1,5 +1,21 @@
 package com.teamenfuego.barcodeshopper.barcodeshopper;
 
+
+        import java.io.IOException;
+        import java.net.URL;
+        import java.net.URLConnection;
+
+        import javax.xml.parsers.DocumentBuilder;
+        import javax.xml.parsers.DocumentBuilderFactory;
+        import javax.xml.parsers.ParserConfigurationException;
+        import javax.xml.transform.TransformerException;
+
+        import org.w3c.dom.Document;
+        import org.w3c.dom.Element;
+        import org.w3c.dom.Node;
+        import org.w3c.dom.NodeList;
+        import org.xml.sax.SAXException;
+
 /**
  * Created by isaac on 4/21/17.
  */
@@ -20,7 +36,75 @@ public class Item {
 
     public Item(int barcodeID) {
         //contact sellers
-        this("default", "$0.00", "default", barcodeID);
+        this("Default", "0.0", "Default", barcodeID);
+
+        String price = "$0.00";
+        String name = "Unnamed item";
+        String seller = "Amazon";
+
+        try {
+            URL url = new URL("http://api.upcdatabase.org/xml/0a4a07f05adbdb4d244054fdfa66aea5/" + barcodeID);
+            URLConnection conn = url.openConnection();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(conn.getInputStream());
+
+            doc.getDocumentElement().normalize();
+
+            //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("output");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    //price is assigned
+                    price = eElement.getElementsByTagName("avgprice").item(0).getTextContent();
+
+
+                    //finds itemname in one of three places or defaults to assignments above
+                    String itemname = eElement.getElementsByTagName("itemname").item(0).getTextContent();
+                    if (itemname != "") {
+                        name = itemname;
+                        assign(name, price, seller);
+                        return;
+                    } else {
+                        itemname = eElement.getElementsByTagName("alias").item(0).getTextContent();
+                        if (itemname != "") {
+                            name = itemname;
+                            assign(name, price, seller);
+                            return;
+                        } else {
+                            itemname = eElement.getElementsByTagName("description").item(0).getTextContent();
+                            if (itemname != "") {
+                                name = itemname;
+                                assign(name, price, seller);
+                                return;
+                            } else {
+                                assign(name, price, seller);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    private void assign(String name, String price, String seller) {
+        this.name = name;
+        this.price = price;
+        this.seller = seller;
     }
 
     public String toString() {
